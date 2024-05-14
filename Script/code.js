e78d916d60a61427b8f55fbe335cc35a70e19246
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-05-14 11:24:17 +0200

    did the exercise

---
+++ Script/code.js
@@ -0,0 +1,22 @@
+
+
+
+let friendslist = [
+    {
+firstname:"f",
+lastname: "g"
+
+    }
+]
+
+
+ function addfriend() {
+let firstName = document.getElementById("Fname").value
+let lastName = document.getElementById("Sname").value
+friendslist.push(firstName, lastName) 
+
+console.log(friendslist);
+
+
+
+}
